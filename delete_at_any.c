ffa49df43a2a8b77da0ea6afc31c78b48db68e19
srikanth0418 <84239219+srikanth0418@users.noreply.github.com>
2021-05-18 12:12:35 +0530

    Add files via upload

---
+++ delete_at_any.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"linkedlist.h"
+void delete_at_any()
+{
+	extern struct node *head,*temp,*temp1;
+	int location,i=1;
+	extern int len_gth;
+	printf("enter the location:\n");
+	scanf("%d",&location);
+	num2=length();
+	if(head=NULL)
+	{
+		printf("given blist is empty\n");
+	}
+	else if(location==0)
+	{
+		delete_at_begin();
+	}
+	else if(location<len_gth-1)
+	{
+		temp=head;
+		while(i<location)
+		{
+			temp1=temp;
+			temp=temp->next;
+			i++;
+		}
+		temp1->next=temp->next;
+		free(temp);
+	}
+	else
+	{
+		printf("given location is invalid\n");
+	}
+}
+
+
+
---
+++ linkedlist.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_H
+#define HEADER-H
+int len_gth;
+struct node
+{
+    int data;
+    struct node *next;
+}*head,*temp,*temp1;
+#include<stdlib.h>
+void add_at_begin();
+void add_at_end();
+void display();
+void add_at_any();
+void delete_at_begin();
+void delete_at_any();
+void delete_at_end();
+int length();
+#endif
+
