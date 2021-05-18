04b2cd384373627da4506cd4138ec38a1190ed60
srikanth0418 <84239219+srikanth0418@users.noreply.github.com>
2021-05-18 12:24:28 +0530

    Update add_at_end.c

--- add_at_end.c
+++ add_at_end.c
@@ -1,6 +1,7 @@
+//*adding at end of the node function*//
 #include<stdio.h>
 #include<stdlib.h>
-#include"inkedlist.h"
+#include"linkedlist.h"
 void add_at_end()
 {
 	int num1;
@@ -8,7 +9,7 @@ void add_at_end()
 	extern struct node *temp,*temp1;
 	temp1=(struct node*)malloc(sizeof(struct node));
 	printf("enter the data:");
-	scanf("%d",&num1);
+	scanf("%d",&num1);    //*enter the data what we have to store in node*//
 	temp1->data=num1;
 	if(head==NULL)
 	{
@@ -18,7 +19,7 @@ void add_at_end()
 	else
 	{
 		temp=head;
-		while(temp->next!=head)
+		while(temp->next!=head)   //*travers the node untill last node come then add at the end *//
 		{
 			temp=temp->next;
 		}
